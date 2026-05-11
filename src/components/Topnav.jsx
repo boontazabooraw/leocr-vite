02fc9b674d482c04abf8e7a059a098ea70d6022d
boontazabooraw <leonard.bryan.email@gmail.com>
2026-05-11 23:08:29 +0800

    CHORE: fixed styling for dark/light mode button on top navigation

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -1,4 +1,6 @@
 import { useTheme } from "../hooks/useTheme";
+import Dark from "../assets/dark.svg?react";
+import Light from "../assets/light.svg?react";
 
 export default function Topnav() {
   const { theme, toggleTheme } = useTheme();
@@ -6,7 +8,13 @@ export default function Topnav() {
   return (
     <div className="flex justify-between p-2 fixed top-0 w-full">
       <h1>&nbsp;</h1>
-      <button className="text-3xl" onClick={toggleTheme}>{theme === "light" ? "🌚" : "🌞"}</button>
+      <button className="p-2 text-(--text)" onClick={toggleTheme}>
+        {theme === "light" ? (
+          <Dark className="w-8 h-8" />
+        ) : (
+          <Light className="w-8 h-8 " />
+        )}
+      </button>
     </div>
   );
 }
