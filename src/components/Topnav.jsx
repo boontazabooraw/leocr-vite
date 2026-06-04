1e7238d0395d48a45f6306058abfd33a4e855907
boontazabooraw <leonard.bryan.email@gmail.com>
2026-06-04 15:30:21 +0800

    CHORE: changed the dark mode toggle icons

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -1,13 +1,13 @@
 import { useTheme } from "../hooks/useTheme";
-import Dark from "../assets/dark.svg?react";
-import Light from "../assets/light.svg?react";
+import Dark from "../assets/moon.svg?react";
+import Light from "../assets/sun.svg?react";
 
 export default function Topnav() {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className="flex justify-between p-4 fixed top-0 w-full">
-      <h1 className="text-center font-semibold font-quicksand text-2xl">LeOCR</h1>
+      <h1 className="text-center font-semibold font-inter text-2xl">LeOCR</h1>
       <button className="text-(--text) hover:opacity-80 transition-all duration-300 " onClick={toggleTheme}>
         {theme === "light" ? (
           <Dark className="w-8 h-8" />
