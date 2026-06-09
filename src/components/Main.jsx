920895207d94ea1cc89081187beef874920bb96c
boontazabooraw <leonard.bryan.email@gmail.com>
2026-06-09 17:05:36 +0800

    FIX: image (slight upscaling/preprocessing) for better output

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,48 @@ import Tesseract from "tesseract.js";
 
 export default function Main() {
   const [image, setImage] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
   const [text, setText] = useState("");
   const [progress, setProgress] = useState(null);
 
   const fileInputRef = useRef(null);
 
-  const runOCR = (file) => {
-    Tesseract.recognize(file, "eng", {
+  const imgPreprocessing = (file) => {
+    return new Promise((res) => {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        // Upscaling
+        const scale = 2;
+        canvas.width = img.width * scale;
+        canvas.height = img.height * scale;
+        ctx.setTransform(scale, 0, 0, scale, 0, 0);
+        ctx.drawImage(img, 0, 0);
+
+        // // Greyscale + Theshold ------- TBD
+        // const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        // const data = imageData.data;
+
+        // for (let i = 0; i < data.length; i += 4) {
+        //   const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        //   const val = avg > 128 ? 255 : 0;
+        //   data[i] = data[i + 1] = data[i + 2] = val;
+        // }
+        // ctx.putImageData(imageData, 0, 0);
+
+        canvas.toBlob((blob) => res(blob), "image/png");
+      };
+      img.src = URL.createObjectURL(file);
+    });
+  };
+
+  const runOCR = async (file) => {
+    const processed = await imgPreprocessing(file);
+    Tesseract.recognize(processed, "eng", {
+      langPath: "/tessdata",
+      tessedit_pegseg_mode: 6,
       logger: (prgrs) => {
         if (prgrs.status === "recognizing text") {
           setProgress(Math.round(prgrs.progress * 100));
@@ -27,14 +62,16 @@ export default function Main() {
     event.preventDefault();
     const file = event.dataTransfer.files[0];
     if (file) {
-      setImage(URL.createObjectURL(file));
+      setImage(file);
+      setImagePreview(URL.createObjectURL(file));
     }
   };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      setImage(URL.createObjectURL(file));
+      setImage(file);
+      setImagePreview(URL.createObjectURL(file));
     }
   };
 
@@ -49,7 +86,8 @@ export default function Main() {
         if (item.type.indexOf("image") !== -1) {
           const file = item.getAsFile();
           if (file) {
-            setImage(URL.createObjectURL(file));
+            setImage(file);
+            setImagePreview(URL.createObjectURL(file));
           }
         }
       }
@@ -91,7 +129,7 @@ export default function Main() {
             </div>
           ) : (
             <img
-              src={image}
+              src={imagePreview}
               className="absolute overflow-none h-full w-full object-cover blur-[2px] saturate-20"
               alt="Uploaded image"
             />
@@ -101,15 +139,18 @@ export default function Main() {
           onClick={() => runOCR(image)}
           disabled={!image || progress === 0 || progress === 100 ? true : false}
           className={`
-            ${image && 'bg-(--text) text-(--bg) hover:bg-(--text)/90'} 
-            border border-(--text) place-self-center-safe min-w-30 min-h-10 rounded-md disabled:pointer-events-none cursor-pointer transition-colors duration-700`}
+            ${image ? "opacity-100" : "opacity-0"} 
+            border border-(--text) place-self-center-safe min-w-30 min-h-10 rounded-md disabled:pointer-events-none cursor-pointer transition-all duration-700`}
         >
           <span>{!progress ? "Process" : progress + "%"}</span>
         </button>
+
+        {/* TEXT AREA */}
+
         <textarea
           className="relative p-2 outline-dashed outline-2 rounded-md min-h-60 xl:min-h-70 min-w-70 flex flex-col justify-center items-center"
           value={text}
-          readOnly
+          readOnly={!image}
         />
       </div>
     </main>
